92b3ec565e0bf757588f1b459d347077f6a7cf6b
Jay M <48458961+jayjmon@users.noreply.github.com>
2021-09-05 15:17:20 -0400

    Update app.js
    added function to update json image url

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
-
+const fs = require("fs");
 const download = require('image-downloader')
-
 let recipes = require('./recipes.json');
-
-const getFileExtension = (filename) =>{
+const path = require("path")
+const getFileExtension = filename =>{
 	return filename.split(".").pop();
 }
 
+
+
+const imageDownloader = () =>{
+	const options = {
+		  url: 'http://someurl.com/image.jpg',
+		  dest: './img/image.jpg',                // will be saved to /path/to/dest/image.jpg
+		  extractFilename: false
+		}
+
+	 for(let i in recipes.Data){
+		
+		image = i.image
+		console.log(i, recipes.Data[i].image[0])
+		options.url = recipes.Data[i].image[0];
+		options.dest = './img/' + i.replaceAll(" ", "-") + "."+getFileExtension(options.url);
+		download.image(options)
+		  .then(({ filename  }) => {
+			console.log('Saved to', filename )  // saved to /path/to/dest/image.jpg
+		  })
+		  .catch((err) => console.error(err))
+	  recipes.Data[i].image = "https://ketonebee.com/" + i.replaceAll(" ", "-") + "."+getFileExtension(options.url);
+	 }
 	
-const options = {
-  url: 'http://someurl.com/image.jpg',
-  dest: './img/image.jpg',                // will be saved to /path/to/dest/image.jpg
-  extractFilename: false
+	console.log(recipes)
+	jsonEditer(recipes);
 }
- for(let i in recipes.Data){
+
+const jsonEditer =(newJsonValues)=>{
+    const fileName = 'recipes.json';
+
+	recipes = newJsonValues;
 	
-    image = i.image
-	console.log(i, recipes.Data[i].image[0])
-	options.url = recipes.Data[i].image[0];
-	options.dest = './img/' + i.replaceAll(" ", "-") + "."+getFileExtension(options.url);
-download.image(options)
-  .then(({ filename  }) => {
-    console.log('Saved to', filename )  // saved to /path/to/dest/image.jpg
-  })
-  .catch((err) => console.error(err))
-  
- }
+	fs.writeFileSync(path.join(__dirname,fileName), JSON.stringify(recipes));
+}
+
+imageDownloader();
